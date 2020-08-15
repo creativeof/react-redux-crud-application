74cc51b941bf36672388862a6e59a0dc129f06a3
creativeof <young_persons_town@yahoo.co.jp>
2020-08-15 20:21:09 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return <div><h1>Hello world!</h1></div>;
+// function App() {
+//   return <div><h1>Hello world!</h1></div>;
+// }
+
+
+// class App extends Component {
+//   render() {
+//     return <div>Hello world!</div>;
+//   }
+// }
+
+// JSXはアプリの内部でトランスファイルという処理が施されて、
+// 以下のようなJavaScriptのコード(React.cerateElement())に変換される。
+
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello world!!"
+//     );
+//   }
+// }
+
+
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi, Tom!";
+//     const dom = <h1 className="foo">{greeting}</h1>;
+//     return dom;
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return <input type="text" onClick={() => {console.log("I am clicked!")}} />;
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am changed!")}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
